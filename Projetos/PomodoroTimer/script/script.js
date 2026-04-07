7d1e10a9c4b96d956d622fc898df81cd6ec7f728
Guilherme Mantoanelli <gbmsom@gmail.com>
2026-04-07 15:58:23 -0300

    .

--- Projetos/PomodoroTimer/script/script.js
+++ Projetos/PomodoroTimer/script/script.js
@@ -1,7 +1,15 @@
+<<<<<<< Updated upstream
 /* const pomodorobtn = document.getElementById("Pomodoro");
+=======
+const pomodorobtn = document.getElementById("Pomodoro");
+>>>>>>> Stashed changes
  = document.getElementById("PausaC");
 = document.getElementById("PausaL");
 const botaoini = document.getElementById("bntstart");
 document.getElementById("Textotxt");
 
+<<<<<<< Updated upstream
 if ("btntmpcomecar") */
+=======
+if ("btntmpcomecar")
+>>>>>>> Stashed changes
