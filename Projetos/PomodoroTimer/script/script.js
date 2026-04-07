a8467540ae1b4ba020ab809092ff1c9356a9399e
Guilherme Mantoanelli <gbmsom@gmail.com>
2026-04-07 15:57:50 -0300

    .

---
+++ Projetos/PomodoroTimer/script/script.js
@@ -0,0 +1,7 @@
+/* const pomodorobtn = document.getElementById("Pomodoro");
+ = document.getElementById("PausaC");
+= document.getElementById("PausaL");
+const botaoini = document.getElementById("bntstart");
+document.getElementById("Textotxt");
+
+if ("btntmpcomecar") */
